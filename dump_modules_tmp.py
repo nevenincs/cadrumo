0ebdcce2bed300180c45d78081d29400f547f2dc
Gergely Wootsch <hello@gergely-wootsch.com>
2026-09-03 03:10:10 +0200

    chore: drop the registry module-import probe scratch

--- dump_modules_tmp.py
+++
@@ -1,4 +0,0 @@
-import json, sys
-from cadrumo.domain.calculations.registry.authority import bundled_authority
-bundled_authority()
-print(json.dumps(sorted(n for n in sys.modules if n.startswith("cadrumo"))))
